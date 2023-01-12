793fa4ce2ad1163400b6d4497b653856ed5ef400
aohanesian <103946712+aohanesian@users.noreply.github.com>
2023-01-12 23:49:26 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from "react";
+
+function App() {
+
+    const [listItem, setListItem] = useState([]);
+    const handlePlusClick = () => setListItem([listItem.length ? listItem[0] + 1 : 1, ...listItem]);
+    const handleMinusClick = () => setListItem([listItem.length ? listItem[0] - 1 : 1, ...listItem]);
+    console.log(listItem)
+    return (
+        <div className="App container mt-5">
+            <div className="btn-group font-monospace" role="group">
+                <button onClick={handlePlusClick} type="button" className="btn btn-outline-success">+</button>
+                <button onClick={handleMinusClick} type="button" className="btn btn-outline-danger">-</button>
+            </div>
+            {listItem.length ? <div className="list-group">
+                {listItem.map((item, index) => <button key={index} type="button"
+                                                       className="list-group-item list-group-item-action">{item}</button>)}
+            </div> : ``}
+        </div>
+    );
+}
+
+export default App;
